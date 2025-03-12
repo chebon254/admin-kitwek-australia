0ef238f141a141c4e6c83c19bbdbd3b922b79f7b
chebon254 <kelvinchebon90@gmail.com>
2025-03-12 23:54:37 +0300

    blog fix

--- src/app/api/blogs/[id]/route.ts
+++ src/app/api/blogs/[id]/route.ts
@@ -4,7 +4,7 @@ import { prisma } from "@/lib/prisma";
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const { userId } = await auth();
@@ -13,7 +13,7 @@ export async function GET(
     }
     
     const blog = await prisma.blog.findUnique({
-      where: { id: params.id },
+      where: { id: (await params).id },
     });
     
     if (!blog || blog.adminId !== userId) {
