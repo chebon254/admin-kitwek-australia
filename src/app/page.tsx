4f613da5b364c86b3a8ea985b2510d32cb9e7e5f
chebon254 <kelvinchebon90@gmail.com>
2025-03-17 14:10:41 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useAuth } from '@clerk/nextjs';
 import { useRouter } from 'next/navigation';
 
